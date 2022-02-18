39087b11f04d3383e4e8dcd04219b1f7dbc1b4e5
Fernando Blasco <fernando@sumun.net>
2022-02-18 12:11:51 +0100

    Blog post container

--- single.php
+++ single.php
@@ -11,31 +11,35 @@ get_header();
 get_template_part( 'template-parts/hero', get_post_type() );
 ?>
 
-<div class="layout-sidebar">
+<div class="container">
 
-	<main id="primary" class="site-main">
+	<div class="layout-sidebar">
 
-		<?php
+		<main id="primary" class="site-main">
 
-		while ( have_posts() ) :
-			the_post();
+			<?php
 
-			get_template_part( 'template-parts/content', get_post_type() );
+			while ( have_posts() ) :
+				the_post();
+
+				get_template_part( 'template-parts/content', get_post_type() );
 
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			//if ( comments_open() || get_comments_number() ) :
-			//	comments_template();
-			//endif;
+				// If comments are open or we have at least one comment, load up the comment template.
+				//if ( comments_open() || get_comments_number() ) :
+				//	comments_template();
+				//endif;
 
-		endwhile; // End of the loop.
-		?>
+			endwhile; // End of the loop.
+			?>
 
-	</main><!-- #main -->
+		</main><!-- #main -->
 
-	<?php get_sidebar(); ?>
+		<?php get_sidebar(); ?>
 
-</div><!-- layout-sidebar -->
+	</div><!-- layout-sidebar -->
+
+</div>
 
 <?php 
 	get_template_part( 'template-parts/related', get_post_type());
