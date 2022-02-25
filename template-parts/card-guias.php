24499cb092e7d81db78444a3e59bf6be63d6615b
Fernando Blasco <fernando@sumun.net>
2022-02-25 02:43:32 +0100

    Cards styles fixes

--- template-parts/card-guias.php
+++ template-parts/card-guias.php
@@ -6,6 +6,6 @@
     <div class="card-content">
         <h2 class="text-h3"><a id="id-<?php the_id(); ?>" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
         <div class="icon is-style-with-icon"><span class="wp-block-button__link"></span></div>
-        <p class="card-excerpt"><?php echo get_the_excerpt();?></p>
+        <!-- <p class="card-excerpt"><?php echo get_the_excerpt();?></p> -->
     </div> 
 </div>
