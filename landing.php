7c81bb9857f3cf63109c9243110509e30aca9d1e
alvarorubioc <alvaro.rubiocamino@gmail.com>
2022-04-07 13:11:02 +0200

    add landing template

---
+++ landing.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * The template for display the Homepage | Frontpage
+ * 
+ * Template Name: Landing
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package innotec
+ */
+
+
+?>
+
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/assets/favicon.png">
+    <link rel="shortcut icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/assets/favicon.png">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'innotec' ); ?></a>
+
+	<header id="masthead" class="site-header container">
+
+		<div class="site-branding">
+			<?php if ( has_custom_logo() ) {
+				the_custom_logo();
+			} else {
+				echo '<a href="'.get_home_url().'" class="custom-logo-link" rel="home" aria-current="page">';
+					echo '<img width="166" height="31" src="'.get_stylesheet_directory_uri().'/assets/logo-innotec.svg" alt="'.get_bloginfo( 'name' ).' '.get_bloginfo( 'description' ).'">';
+				echo '</a>';
+			} ?>
+		</div><!-- .site-branding -->
+		
+		<div class="site-btn--contact"><a href="/contacto/"><span class="screen-reader-text"><?php esc_html_e( 'Contacto', 'innotec' ); ?></span></a></div>
+	</header><!-- #masthead -->
+
+
+	<main id="primary" class="site-main">
+
+		<?php
+
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+
+<?php
+get_footer();
---
+++ template-parts/hero-landing.php
@@ -0,0 +1,31 @@
+<?php if ( has_post_thumbnail() ) : 
+    $get_image_url = wp_get_attachment_url( get_post_thumbnail_id() );
+    $image_background = 'background-image: url('.$get_image_url.')';
+endif; ?>
+
+<header id="hero" class="entry-header" style="<?php echo $image_background ;?>">
+    <div class="container">
+        <?php
+            the_title( '<h1 class="display">', '</h1>' );
+            if ( has_excerpt() ) :
+                $excerpt = get_the_excerpt();
+                echo '<h2>'.$excerpt.'</h2>';
+             endif;
+        ?>
+        <?php 
+        $link_1 = get_field('hero_link');
+        if( $link_1 ): 
+            $link_url = $link_1['url'];
+            $link_title = $link_1['title'];
+            $link_target = $link_1['target'] ? $link_1['target'] : '_self';
+            ?>
+            <div class="icon is-style-with-icon mt-2"><a class="wp-block-button__link" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a></div>
+        <?php endif; ?>
+    </div>   
+</header><!-- #hero -->
+
+<div id="breadcrumbs">
+    <div class="container">
+        <?php if (function_exists('rank_math_the_breadcrumbs')) rank_math_the_breadcrumbs(); ?>
+    </div>
+</div><!-- #breadcrumbs -->
